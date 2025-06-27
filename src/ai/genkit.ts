adb672f0f4ac39c564d2093ed2d7c92ecd4d02e4
Dra Mull <153959031+dramull@users.noreply.github.com>
2025-06-27 14:33:14 -0500

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI({
-    apiKey: process.GEMINI_API_KEY || process.GOOGLE_GENAI_API_KEY
+    apiKey: process.env.GEMINI_API_KEY || process.env.GOOGLE_GENAI_API_KEY
   })],
   model: 'googleai/gemini-2.0-flash',
   // Add these two lines for Netlify
