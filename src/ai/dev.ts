48cb2c2434b5b1c50cd7aa187839f08a94041123
Dra Mull <153959031+dramull@users.noreply.github.com>
2025-06-27 14:53:12 -0500

    Delete src/ai/dev.ts

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-// This file is causing flows to register at module load time
-// which triggers the dev server
-// import { config } from 'dotenv';
-// config();
-// import '@/ai/flows/text-to-speech.ts';
-// import '@/ai/flows/generate-motivational-message.ts';
